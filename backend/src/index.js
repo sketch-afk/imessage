fd31326132a190c79cd9f900ff24be9e46c850a0
Yash <mvmoraiyashraj10d@gmail.com>
2026-06-15 16:29:50 +0530

    Add Dockerfile and .dockerignore; implement static file serving in Express

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,11 @@
 import express from 'express'
 import cors from 'cors';
+
 import "dotenv/config"
+
+import fs from 'fs';
+import path from 'path';
+
 
 import { clerkMiddleware } from '@clerk/express'
 
@@ -11,6 +16,8 @@ const app = express()
 const PORT = process.env.PORT
 const FRONTEND_URL = process.env.FRONTEND_URL
 
+const publicDir = path.join(process.cwd(), "public");
+
 app.use(express.json())
 app.use(cors({origin:FRONTEND_URL, credentials:true}))
 app.use(clerkMiddleware())
@@ -19,7 +26,31 @@ app.get("/health", (req, res)=>{
   res.status(200).json({ok:true})
 })
 
+// if the public directory exist, serve the static files
+// this is for the production build
+if(fs.existsSync(publicDir)){
+  app.use(express.static(publicDir))
+
+  app.get("*", (req, res, next)=>{
+    res.sendFile(path.join(publicDir, "index.html"), (err)=> next(err));
+  });
+}
+
 app.listen(PORT, () => {
   connectDB();
   console.log(`Server is listening on port ${PORT}`)
 })
+
+// const startServer = async () => {
+//   try {
+//     await connectDB();
+//     app.listen(PORT, () => {
+//       console.log(`Server is listening on port ${PORT}`)
+//     })
+//   } catch (error) {
+//     console.error("Database connection failed. Server not started.", error);
+//     process.exit(1);
+//   }
+// }
+
+// startServer();
