7776c048d47829e547e4e27b0f2fae186bbdb8e3
Yash <mvmoraiyashraj10d@gmail.com>
2026-06-15 16:43:57 +0530

    Fix route handling for static file serving in Express

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,7 +31,7 @@ app.get("/health", (req, res)=>{
 if(fs.existsSync(publicDir)){
   app.use(express.static(publicDir))
 
-  app.get("*", (req, res, next)=>{
+  app.get("/{*any}", (req, res, next)=>{
     res.sendFile(path.join(publicDir, "index.html"), (err)=> next(err));
   });
 }
